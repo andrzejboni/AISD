9eb3c754e6e05f0d4d5f4bba01c4ff9a16471e07
andrzejboni <andrzej.boni@outlook.com>
2018-09-20 20:59:25 +0200

    Bubble sort >

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 import exercise.AlgorithmExercise;
+import exercise.SortingExercise;
 import impl.AlgorithmExerciseImpl;
+import impl.SortingExerciseImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -8,17 +13,30 @@ public class Main {
         int tab[] = new int[] {5,5,5,5,6,21};
 
 
-        AlgorithmExercise algorithmExercise = new AlgorithmExerciseImpl();
+//        AlgorithmExercise algorithmExercise = new AlgorithmExerciseImpl();
+//
+//        System.out.println("NWD to " +algorithmExercise.nwd(88,16));
+//        System.out.println("NWD Recursive to " +algorithmExercise.nwdRecursive(88,16));
+//        System.out.println("SILNIA z liczby to " + algorithmExercise.silnia(11));
+//        System.out.println("Suma tablicy list " + algorithmExercise.suma(tab));
+//        System.out.println("Suma Recursive tablicy list " + algorithmExercise.sumaRecursive(tab));
+//        System.out.println("Nr liczby fibbonaciego to " + algorithmExercise.fibonacci(5));
+//        System.out.println("Nr liczby fibbonaciego to " + algorithmExercise.fibonacciRecursive(5));
+//        System.out.println( algorithmExercise.reverse("ggrzyb"));
 
-        System.out.println("NWD to " +algorithmExercise.nwd(88,16));
-        System.out.println("NWD Recursive to " +algorithmExercise.nwdRecursive(88,16));
-        System.out.println("SILNIA z liczby to " + algorithmExercise.silnia(11));
-        System.out.println("Suma tablicy list " + algorithmExercise.suma(tab));
-        System.out.println("Suma Recursive tablicy list " + algorithmExercise.sumaRecursive(tab));
-        System.out.println("Nr liczby fibbonaciego to " + algorithmExercise.fibonacci(5));
-        System.out.println("Nr liczby fibbonaciego to " + algorithmExercise.fibonacciRecursive(5));
-        System.out.println( algorithmExercise.reverse("ggrzyb"));
 
+        SortingExerciseImpl s1 = new SortingExerciseImpl();
+
+        ArrayList list = new ArrayList();
+        list.add(12);
+        list.add(3);
+        list.add(87);
+        list.add(15);
+        list.add(119);
+        list.add(1);
+
+        s1.bubbleSort(list);
+        System.out.println(s1.bubbleSort(list));
 
 
     }
--- src/main/java/impl/SortingExerciseImpl.java
+++ src/main/java/impl/SortingExerciseImpl.java
@@ -2,6 +2,7 @@ package impl;
 
 import exercise.SortingExercise;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SortingExerciseImpl implements SortingExercise {
@@ -9,11 +10,25 @@ public class SortingExerciseImpl implements SortingExercise {
 
     @Override
     public List<Integer> bubbleSort(List<Integer> unsorted) {
-        return null;
+        int a;
+        int b;
+
+        for (int j = 0; j < unsorted.size(); j++) {
+            for (int i = 0; i < unsorted.size() - 1; i++) {
+                if (unsorted.get(i) > unsorted.get(i + 1)) {
+                    a = unsorted.get(i);
+                    b = unsorted.get(i + 1);
+                    unsorted.set(i, b);
+                    unsorted.set(i + 1, a);
+                }
+            }
+        }
+        return unsorted;
     }
 
     @Override
     public List<Integer> insertSort(List<Integer> unsorted) {
+
         return null;
     }
 
