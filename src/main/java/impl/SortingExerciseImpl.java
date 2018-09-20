ef25041b244515a33112af597238227b4f860951
andrzejboni <andrzej.boni@outlook.com>
2018-09-20 20:28:21 +0200

    algorytmy sortujace klasa ->

---
+++ src/main/java/impl/SortingExerciseImpl.java
@@ -0,0 +1,34 @@
+package impl;
+
+import exercise.SortingExercise;
+
+import java.util.List;
+
+public class SortingExerciseImpl implements SortingExercise {
+
+
+    @Override
+    public List<Integer> bubbleSort(List<Integer> unsorted) {
+        return null;
+    }
+
+    @Override
+    public List<Integer> insertSort(List<Integer> unsorted) {
+        return null;
+    }
+
+    @Override
+    public List<Integer> selectionSort(List<Integer> unsorted) {
+        return null;
+    }
+
+    @Override
+    public List<Integer> mergeSort(List<Integer> unsorted) {
+        return null;
+    }
+
+    @Override
+    public List<Integer> quickSort(List<Integer> unsorted) {
+        return null;
+    }
+}
