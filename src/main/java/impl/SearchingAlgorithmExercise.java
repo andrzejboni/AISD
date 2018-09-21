98be404d962052873a0dc594309885b0bcf0e489
andrzejboni <andrzej.boni@outlook.com>
2018-09-21 20:51:05 +0200

    algorytm wyszukiwania p2

--- src/main/java/impl/SearchingAlgorithmExercise.java
+++ src/main/java/impl/SearchingAlgorithmExercise.java
@@ -8,10 +8,23 @@ public class SearchingAlgorithmExercise {
         boolean isFoud = false;
 
         elements = new SortingExerciseImpl().insertSort2(elements);
+        int middle = (elements.size() + 1) / 2;
 
 
+        while (middle != 0) {
+
+            if (elements.get(middle) == searchedElement) {
+                isFoud = true;
+                break;
+            } else if (elements.get(middle) > searchedElement) {
+                elements = elements.subList(middle + 1, elements.size() - 1);
+
+            } else {
+                elements = elements.subList(0,middle-1);
+            }
 
 
+        }
         return true;
     }
 
