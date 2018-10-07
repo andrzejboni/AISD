2e9c45b7a76da3f55b4eb77abac9912fce79169d
andrzejboni <andrzej.boni@outlook.com>
2018-10-07 15:31:11 +0200

    .

--- src/main/java/impl/AlgorithmExerciseImpl.java
+++ src/main/java/impl/AlgorithmExerciseImpl.java
@@ -91,13 +91,10 @@ public class AlgorithmExerciseImpl implements AlgorithmExercise {
     }
 
     public int fibonacciRecursive(int n) {
-
         if (n == 0) return 0;
         if (n == 1) return 1;
 
         return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
-
-
     }
 
     public String reverse(String text) { // Tekst wspak!
